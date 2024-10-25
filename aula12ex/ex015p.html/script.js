2c00f731004438997f6f360e72d8208ea6f2e75d
josehenriquedasilva <josehenrique60t@gmail.com>
2024-10-25 10:56:05 -0300

    Exercícios corrigidos

--- aula12ex/ex015p.html/script.js
+++ aula12ex/ex015p.html/script.js
@@ -1,20 +1,41 @@
 function verificar() {
-    var verif = document.querySelector('input#verificar')
     var img = document.querySelector('img#imagem')
     var nasc = document.querySelector('input#nas')
     var idade = Number(nasc.value)
     var info = document.querySelector('section#info')
     var res = 2024 - idade
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
     if (nasc.value == 0 || (nasc.value > 2024)) {
         alert('ERRO')
-    } else {
+    }  else {
         var sex = document.getElementsByName('sexo')
         var gênero = ''
         if (sex[0].checked) {
-            var gênero = 'Masculino'
+            gênero = 'Homem'
+            if (res >= 0 && res < 10) {
+                img.setAttribute('src', 'homem-crianca.jpg')
+            } else if (res < 25) {
+                img.setAttribute('src', 'homem-jovem.webp')
+            } else if (res < 45) {
+                img.setAttribute('src', 'homem-adulto.jpeg')
+            } else {
+                img.setAttribute('src', 'homem-idoso.jpeg')
+            }
         } else if (sex[1].checked) {
-            var gênero = 'Feminino'
+            gênero = 'Mulher'
+            if (res >= 0 && res < 10) {
+                img.setAttribute('src', 'mulher-criança.jpg')
+            } else if (res < 25) {
+                img.setAttribute('src', 'mulher-jovem.webp')
+            } else if (res < 45) {
+                img.setAttribute('src', 'mulher-adulta.jpeg')
+            } else {
+                img.setAttribute('src', 'mulher-idosa.webp')
+            }
         }
-        info.innerHTML = `Detectamos ${sex} de ${res}!`
     }
+    info.style.textAlign = 'center'
+    info.innerHTML = `<p>Detectamos ${gênero} de ${res} anos!</p>`
+    info.appendChild(img)
 }
